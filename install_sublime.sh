7dd16dd563a8e46fe2d042917d4b51cd2383594f
Christoph Baumann <christoph-baumann@mail.de>
2021-08-23 14:37:00 +0200

    fixed install script

--- install_sublime.sh
+++ install_sublime.sh
@@ -53,12 +53,12 @@ then
 fi
 
 if ! (patchelf --remove-rpath /opt/sublime_text/sublime_text && \
-      patchelf --remove-rpath /opt/sublime_text/plugin_host-3.3 \
-      patchelf --remove-rpath /opt/sublime_text/plugin_host-3.8 \
-      patchelf --remove-rpath /opt/sublime_text/crash_reporter \
-      patchelf --force-rpath --set-rpath "/opt/glibc2.28/lib:/usr/lib/aarch64-linux-gnu:/lib/aarch64-linux-gnu:\$ORIGIN" --set-interpreter /opt/glibc2.28/lib/ld-linux-aarch64.so.1 /opt/sublime_text/sublime_text \
-      patchelf --force-rpath --set-rpath "/opt/glibc2.28/lib:/usr/lib/aarch64-linux-gnu:/lib/aarch64-linux-gnu:\$ORIGIN" --set-interpreter /opt/glibc2.28/lib/ld-linux-aarch64.so.1 /opt/sublime_text/plugin_host-3.3 \
-      patchelf --force-rpath --set-rpath "/opt/glibc2.28/lib:/usr/lib/aarch64-linux-gnu:/lib/aarch64-linux-gnu:\$ORIGIN" --set-interpreter /opt/glibc2.28/lib/ld-linux-aarch64.so.1 /opt/sublime_text/plugin_host-3.8 \
+      patchelf --remove-rpath /opt/sublime_text/plugin_host-3.3 && \
+      patchelf --remove-rpath /opt/sublime_text/plugin_host-3.8 && \
+      patchelf --remove-rpath /opt/sublime_text/crash_reporter && \
+      patchelf --force-rpath --set-rpath "/opt/glibc2.28/lib:/usr/lib/aarch64-linux-gnu:/lib/aarch64-linux-gnu:\$ORIGIN" --set-interpreter /opt/glibc2.28/lib/ld-linux-aarch64.so.1 /opt/sublime_text/sublime_text && \
+      patchelf --force-rpath --set-rpath "/opt/glibc2.28/lib:/usr/lib/aarch64-linux-gnu:/lib/aarch64-linux-gnu:\$ORIGIN" --set-interpreter /opt/glibc2.28/lib/ld-linux-aarch64.so.1 /opt/sublime_text/plugin_host-3.3 && \
+      patchelf --force-rpath --set-rpath "/opt/glibc2.28/lib:/usr/lib/aarch64-linux-gnu:/lib/aarch64-linux-gnu:\$ORIGIN" --set-interpreter /opt/glibc2.28/lib/ld-linux-aarch64.so.1 /opt/sublime_text/plugin_host-3.8 && \
       patchelf --force-rpath --set-rpath "/opt/glibc2.28/lib:/usr/lib/aarch64-linux-gnu:/lib/aarch64-linux-gnu:\$ORIGIN" --set-interpreter /opt/glibc2.28/lib/ld-linux-aarch64.so.1 /opt/sublime_text/crash_reporter)
 then
 	echo "Failed to patch Sublime Text binaries. Exiting."
